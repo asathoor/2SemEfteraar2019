124f4de3a5ec4807ae4b7bae9b0436669b874035
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-08-26 09:35:43 +0200

    I klassen
    pause e en time

---
+++ intro-til-js/js/js.js
@@ -0,0 +1,18 @@
+/**
+ * file: js.js
+ * purpose: interactivity
+ **/
+
+/* hvad er en variabel? */
+let hilsen = 'Hej verden - nu fra en variabel!'; // variabel
+var hej = "Hej ven"; // variabel
+const adresse = 'Ringvej Syd 104<br>8260 Viby J'; // konstant
+
+// virker mit script?
+console.log(adresse);
+
+// tal
+let aaa = 234;
+
+// JavaScript kan lave beregninger
+let bbb = aaa * 777;
