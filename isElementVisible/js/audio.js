ff39eb49f65f5f1efbc33711423e8d5812f2b799
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-08-29 10:58:57 +0200

    tilføjer diverse dom relaterede mapper

--- isElementVisible/js/audio.js
+++ isElementVisible/js/audio.js
@@ -2,13 +2,20 @@
  * fil: audio.js
  * purpose: play audio when the containing div is visible
  **/
+
+// eventlistener sample
+btn1.addEventListener("click", function() {
+  btn1.style.backgroundColor = "red";
+});
+
+// eventlistener: a window scroll
 window.addEventListener("scroll", function() { // detect scroll event
 
   var mySound = document.getElementById("myaudio"); // select audio id
   var lyd = document.getElementById("lyd");
   console.log("Er div #lyd synlig? " + elFllVsbl(lyd)); // test
 
-// Below: start and stop audio 
+// Below: start and stop audio
   if (elFllVsbl(lyd)) { // if the #lyd div is visible then
     if (!(mySound.curentTime > 0)) {
       mySound.play(); // play audio
@@ -23,7 +30,6 @@ window.addEventListener("scroll", function() { // detect scroll event
 function elFllVsbl(el) {
   return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
 }
-
 /*
 NB: the audio / video tags will allways return true,
 solution: place the tag in a div and test for the div in stead
--- isElementVisible/music/README.md
+++
@@ -1,5 +0,0 @@
-Add Your Music Files Here
-=========================
-
-Add whatever music you use here.
-Remember to change the filename in the code.
